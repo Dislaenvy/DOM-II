b8c18b184e04bb18ce4029a73f72a14a196e5181
Merfi Disla <merfydisla15@gmail.com>
2021-03-12 19:50:58 -0500

    Still working on it

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+const mainNavigation = document.querySelectorAll('.main-navigation')
+const container = document.querySelectorAll('.container home')
+const contentSection = document.querySelectorAll('.content-section')
+const mimgs = document.querySelectorAll('img')
+const texContent = document.querySelectorAll('.tex-content')
+const contentDestination = document.querySelectorAll('.content-destination')
+const contentPick = document.querySelectorAll('.contentPick')
+
+Array.from(document.links).forEach(function (link){
+    link.addEventListener('click', function(event){
+        event.preventDefault();
+        console.log(`not target by default "${event.target.texContent}" link`)
+    })
+})
+
+
+mainNavigation.addEventListener('click', function (event) => {
+   
+})
